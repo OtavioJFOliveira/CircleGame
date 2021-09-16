45f4253b98782ada2fccbe62bb8ccd5360f5ae6f
OtavioJFOliveira <otaviojf_oliveira@outlook.com>
2021-09-15 22:44:37 -0300

    first test _ playing with a circle

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from random import randint
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+w, h = 1280, 720
+cap.set(3, w)
+cap.set(4, h)
 
 circleColor = 255,0,0
 circleX, circleY, circleRad = 200, 200, 100
 
-handDetector = HandDetector(detectionCon=8.0)
+handDetector = HandDetector(detectionCon=0.8)
 
 while True:
     status, img = cap.read()
     img = cv2.flip(img, 1)
+    img = handDetector.findHands(img)
+    list, _ = handDetector.findPosition(img)
+
+    if list:
+        fingerPoint = list[8]
+
+        if (circleX - circleRad ) < fingerPoint[0] < (circleX + circleRad) and (circleY - circleRad) < fingerPoint[1]< (circleY + circleRad):
+            circleColor = 0, 0, 255
+            circleX = randint(circleRad, w-circleX)
+            circleY = randint(circleRad, h-circleY)
+
+        else:
+            circleColor = 255, 0, 0
 
     cv2.circle(img, (circleX,circleY), circleRad, circleColor, cv2.FILLED)
     cv2.imshow("Image", img)
-    circleY = circleY+2
+    circleY = circleY+1
     cv2.waitKey(1)
 
